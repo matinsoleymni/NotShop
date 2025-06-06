dbf4d7d6dfec5c846983b121332aae58928f9141
Matin Soleymani <matinsoleymni@gmail.com>
2025-06-07 01:30:49 +0330

    fix: image zoom in touch

--- app/components/ZoomableImageModalContent.tsx
+++ app/components/ZoomableImageModalContent.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 interface ZoomableImageModalContentProps {
   imageUrl: string;
@@ -8,8 +8,17 @@ interface ZoomableImageModalContentProps {
 const ZoomableImageModalContent: React.FC<ZoomableImageModalContentProps> = ({ imageUrl, onClose }) => {
   const [zoomLevel, setZoomLevel] = useState(1);
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isPanning, setIsPanning] = useState(false);
+  const [startPanPosition, setStartPanPosition] = useState({ x: 0, y: 0 });
   const imageRef = useRef<HTMLImageElement>(null);
   const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    // Reset zoom and position when image changes
+    setZoomLevel(1);
+    setPosition({ x: 0, y: 0 });
+  }, [imageUrl]);
+
 
   const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
     if (zoomLevel > 1 && imageRef.current && containerRef.current) {
@@ -29,6 +38,32 @@ const ZoomableImageModalContent: React.FC<ZoomableImageModalContentProps> = ({ i
     }
   };
 
+  const handleTouchStart = (event: React.TouchEvent<HTMLDivElement>) => {
+    if (zoomLevel > 1 && event.touches.length === 1) {
+      setIsPanning(true);
+      setStartPanPosition({ x: event.touches[0].clientX, y: event.touches[0].clientY });
+    }
+  };
+
+  const handleTouchMove = (event: React.TouchEvent<HTMLDivElement>) => {
+    if (isPanning && event.touches.length === 1) {
+      const deltaX = event.touches[0].clientX - startPanPosition.x;
+      const deltaY = event.touches[0].clientY - startPanPosition.y;
+
+      setPosition(prevPosition => ({
+        x: prevPosition.x + deltaX,
+        y: prevPosition.y + deltaY,
+      }));
+
+      setStartPanPosition({ x: event.touches[0].clientX, y: event.touches[0].clientY });
+    }
+  };
+
+  const handleTouchEnd = () => {
+    setIsPanning(false);
+  };
+
+
   const handleClick = () => {
     setZoomLevel(prevZoom => (prevZoom === 1 ? 2 : 1));
     setPosition({ x: 0, y: 0 }); // Reset position on zoom change
@@ -39,6 +74,9 @@ const ZoomableImageModalContent: React.FC<ZoomableImageModalContentProps> = ({ i
       ref={containerRef}
       className="relative w-full h-full flex items-center justify-center overflow-hidden"
       onMouseMove={handleMouseMove}
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
       onClick={(event) => {
         if (imageRef.current && !imageRef.current.contains(event.target as Node)) {
           onClose();
