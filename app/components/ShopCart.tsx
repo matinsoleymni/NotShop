f5ae9a733b7da38720f4b99cf1a8f32bb507f8ad
Matin Soleymani <matinsoleymni@gmail.com>
2025-06-07 10:26:25 +0330

    fix: layout shift

--- app/components/ShopCart.tsx
+++ app/components/ShopCart.tsx
@@ -97,6 +97,8 @@ export default function ShopCart({
                         className="rounded-2xl aspect-square object-cover"
                         src={images[currentImageIndex]}
                         alt={name}
+                        width={160}
+                        height={160}
                     />
                     <div className="absolute top-2 right-2 dark:invert-100 rounded-full">
                         {isInCart && <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
